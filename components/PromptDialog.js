0e05a2a2c332f04db75724b5a5cbffcd8e56c674
Camila Prá <camila@guiprav.com>
2025-11-25 11:12:09 -0300

    .

---
+++ components/PromptDialog.js
@@ -0,0 +1,9 @@
+export default class PromptDialog {
+	constructor(props) { this.props = props }
+  keyDown: ev => {
+    if (ev.key !== 'Enter' || !this.props.value.trim()) return;
+    this.ok();
+  };
+  cancel: () => { this.root.parentElement.close('cancel') };
+  ok: () => { this.root.parentElement.returnDetail = this.props.value.trim(); this.root.parentElement.close('ok') };
+};
